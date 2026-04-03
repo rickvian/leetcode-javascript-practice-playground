f28f5174ae7caba8fe7c9e2698466cdf43117918
rickvianaldi <rickvianaldi@gmail.com>
2026-04-03 21:14:40 +0700

    refactor: enhance tree-related algorithms with detailed comments and improved logic

--- leetcode-playground/0128-longest-consecutive-sequence.js
+++ leetcode-playground/0128-longest-consecutive-sequence.js
@@ -7,6 +7,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function (nums) {};
+var longestConsecutive = function (nums) {
+  let numSet = new Set(nums);
+  let longestStreak = 0;
+
+  for (num of nums) {
+    if (!numSet.has(num - 1)) {
+      // its start of new chain,
+
+      let currentNum = num;
+      let currentStreak = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+
+      longestStreak = Math.max(currentStreak, longestStreak);
+    }
+  }
+
+  return longestStreak;
+};
 
 export { longestConsecutive };
--- leetcode-playground/0572-subtree-of-another-tree.js
+++ leetcode-playground/0572-subtree-of-another-tree.js
@@ -19,8 +19,24 @@ class TreeNode {
     }
 }
 
-var isSubtree = function(root, subRoot) {
-    // stub
+var isSubtree = function (root, subRoot) {
+    if (subRoot === null) return true // null is always subtree of any tree
+
+    // else, subroot is not null:
+    if (root === null) return false // not null will never be subtree of null.
+
+    if (isSameTree(root, subRoot)) return true
+
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+
+    function isSameTree(tree1, tree2) {
+        if (tree1 === null && tree2 === null) return true
+
+        // potential get one as null, one is not, so they not the same
+        if (tree1 === null || tree2 === null) return false
+
+        return (tree1.val === tree2.val) && isSameTree(tree1.left, tree2.left) && isSameTree(tree1.right, tree2.right)
+    }
 };
 
 export { TreeNode, isSubtree };
