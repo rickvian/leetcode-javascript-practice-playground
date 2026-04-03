376d2b4ee97d7ad2dcc581da46284baa50d8c9ac
rickvianaldi <rickvianaldi@gmail.com>
2026-04-03 19:10:13 +0700

    287 find duplicate

---
+++ leetcode-playground/0287-find-the-duplicate-number.js
@@ -0,0 +1,14 @@
+/**
+ * Given an array of n + 1 integers where each integer is in the range [1, n] inclusive,
+ * return the only number that appears more than once.
+ *
+ * https://leetcode.com/problems/find-the-duplicate-number/description/
+ *
+ * @param {number[]} nums - Array of n + 1 integers in range [1, n]
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    // implement
+};
+
+export { findDuplicate }
