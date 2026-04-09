71e4b101f8aa6f15909d9d9a8b6e37bbbdc3a811
rickvianaldi <rickvianaldi@gmail.com>
2026-04-09 22:27:58 +0700

    Implement uniquePaths function and add detailed comments for clarity

--- leetcode-playground/0062-unique-paths.js
+++ leetcode-playground/0062-unique-paths.js
@@ -6,8 +6,49 @@
  * @param {number} n - Number of columns in the grid
  * @return {number} Number of unique paths from top-left to bottom-right
  */
-var uniquePaths = function(m, n) {
+var uniquePaths = function (m, n) { // 2 x 3
     // Solution implementation
+
+    // intuition, we can take look at the problem at cell level
+
+
+    // [3+3 =9 ] [2+1=3]   [1]
+    // [1+2= 3] [1+1=2]   [1]
+    // [1]        [1]    [1- Goal]
+
+    // can only go right and bottom
+    // reverse engineer make it easier
+
+    // pattern observed:
+    // bottom row always 1
+    // last column always 1
+
+    // other cell is always bottom possible path + right possible path
+
+
+
+    // []        []    [Goal]
+    let resultRow = new Array(n).fill(1)
+
+    for (let i = 0; i < m - 1; i++) {
+        // iterate through all row except, exclude last one.
+        let newRow = new Array(n).fill(1)
+        // iterate for every cell in col
+        for (let j = n - 2; j > -1; j--) {
+            // iterate from last col, start from 2nd last, then slowly build to first in col
+            newRow[j] = newRow[j + 1] + resultRow[j]
+
+            //  j
+            // [9]  [3]  [1] resultRow
+        }
+
+        resultRow = newRow // update the row with new row we calculate, since we only want to calculate the path, we don't need to keep all the newRow we made, we can store update the row and move next iteration
+    }
+
+    // we have first row calculated backwards from bottom row, with the answer
+
+    return resultRow[0]
+
 };
 
 export { uniquePaths }
--- leetcode-playground/0208-implement-trie.js
+++ leetcode-playground/0208-implement-trie.js
@@ -7,7 +7,12 @@
  * https://leetcode.com/problems/implement-trie-prefix-tree/description/
  */
 class Trie {
+
     constructor() {
+        // whats in trie
+
+        this.children = {} // mapping the children character
+        this.isEndOfWord = false
     }
 
     /**
@@ -15,6 +20,28 @@ class Trie {
      * @return {void}
      */
     insert(word) {
+        // trie is tree data structure.
+        //    a    b    c
+        //         ^
+
+        // rootTrie() 
+        // children [a]:Trie()
+        // children [b]:Trie()
+        // children [c]:Trie()<-
+
+        let curr = this // initiate as root
+
+        for (let c of word) {
+            if (!curr.children[c]) {
+                // not exist, add c as children
+                curr.children[c] = new Trie()
+            }
+
+            curr = curr.children[c] // move pointer to inner trie
+        }
+
+        curr.isEndOfWord = true
+
     }
 
     /**
@@ -22,6 +49,31 @@ class Trie {
      * @return {boolean}
      */
     search(word) {
+
+        // search
+        //    g    b    c
+        //              ^
+
+        // value
+        // rootTrie() 
+        // children [g]:Trie()
+        // children [b]:Trie()
+        // children [c]:Trie()  <- curr
+        // children [d]:Trie()  ?
+
+        let curr = this
+
+        for (let c of word) {
+            if (!curr.children[c]) {
+                return false // character does not exist, return false
+            }
+
+            // else, found, continue
+            curr = curr.children[c] // move pointer to inner trie
+
+        }
+
+        return curr.isEndOfWord
     }
 
     /**
@@ -29,6 +81,19 @@ class Trie {
      * @return {boolean}
      */
     startsWith(prefix) {
+        let curr = this
+
+        for (let c of prefix) {
+            if (!curr.children[c]) {
+                return false // character does not exist, return false
+            }
+
+            // else, found, continue
+            curr = curr.children[c] // move pointer to inner trie
+
+        }
+
+        return true
     }
 }
 
