63098b5cfdad9ae860b2a3dae9ba026947c95198
rickvianaldi <rickvianaldi@gmail.com>
2026-04-15 20:59:52 +0700

    Implement coinChange function with dynamic programming; enhance comments for clarity and understanding of the algorithm

--- leetcode-playground/0322-coin-change.js
+++ leetcode-playground/0322-coin-change.js
@@ -10,8 +10,34 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-    // stub
+var coinChange = function (coins, amount) {
+  // stub
+  // we can explore the coins combination by building decision tree
+  // notice that in different decision branch to find minimum coin, we have leftover amount,
+  //  that resolves to same amount of minimum coins
+
+  let dp = Array.from({ length: amount + 1 }, () => amount + 1); // unreachable value for minimum coin, useful later, to check if we don't have possible coin combination
+
+  // set initial value
+
+  dp[0] = 0;
+
+  for (let a = 1; a < amount + 1; a++) {
+    // we building up dp solution to the target amount.
+
+    for (let c of coins) {
+      //
+      if (a - c >= 0) {
+        // process if the amount have leftover only after picking this coin
+
+        dp[a] = Math.min(dp[a], 1 + dp[a - c]); // check dp[a], if this dp[a] is replaceable with 1 coin a-c and is actually have better solution to this leftover?
+      }
+    }
+  }
+
+  if (dp[amount] === amount + 1) return -1;
+
+  return dp[amount];
 };
 
 export { coinChange };
