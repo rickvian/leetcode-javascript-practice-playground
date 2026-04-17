20577034dbe6c052bb0204502cd688001b5450d1
rickvianaldi <rickvianaldi@gmail.com>
2026-04-17 16:23:24 +0700

    add more case

---
+++ leetcode-playground/0216-combination-sum-iii.js
@@ -0,0 +1,18 @@
+/**
+ * Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+ * - Only numbers 1 through 9 are used.
+ * - Each number is used at most once.
+ * Return a list of all possible valid combinations. The list must not contain the same combination twice,
+ * and the combinations may be returned in any order.
+ *
+ * https://leetcode.com/problems/combination-sum-iii/description/
+ *
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function(k, n) {
+    // implement
+};
+
+export { combinationSum3 };
