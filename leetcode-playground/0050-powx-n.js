b267d49076589209328a6c4c6d3265f4c5a9f56e
rickvianaldi <rickvianaldi@gmail.com>
2026-04-22 11:29:05 +0700

    🔵feat: Improve myPow function with enhanced comments for clarity; refine logic and handle edge cases in recursion

--- leetcode-playground/0050-powx-n.js
+++ leetcode-playground/0050-powx-n.js
@@ -11,31 +11,40 @@
  * Space Complexity: O(log n) — recursion stack depth is log n
  */
 var myPow = function (x, n) {
+  // function that x power to n
+  // there are 2 cases, positive and negative number
+  // lets handle positive first
+  // simples approach is num = num * x loop n  times, but its o(n) and not what the question expect.
+  // so there is another approach to powering number with less operation
+  // its to divide and conquer.
+  // say 2^10 we can split into 2^5 * 2^5 , and then 2^3 * 2^2 * 2^3
+  // effectively halve it until base case, and multiply them
+  // the operation keep halving, so it will result in o log N operation
+
   function helper(x, n) {
     // base case
-    if (x == 0) return 0; // 0^any = 0
-    if (n == 0) return 1; // any^0 = 1
+    if (x === 0) return 0;
+    if (n === 0) return 1;
 
-    let res = helper(x, Math.floor(n / 2)); // divide multiplication into 2 part.
-    res = res * res; // merge 2 part
+    // divide and conquer
 
-    // handle special case of odd, multiply 1 more time
+    let res = helper(x, Math.floor(n / 2)); // flooring it mean we divide take the even number,
+    res = res * res; // merge it back
+
     if (n % 2 === 1) {
+      // say 2^3, odd power because we floored it, we handle the extra 1 more multiplicationhere
       return res * x;
     }
-    // else, even number
 
+    // else - even
     return res;
   }
 
-  // n can be negative, we process them same first
-  let res = helper(x, Math.abs(n));
+  let result = helper(x, Math.abs(n)); // we care calculate the positive first
 
-  // decide if negative, 1 over it, else return res
+  if (n < 0) return 1 / result; // per math definition
 
-  if (n < 0) return 1 / res;
-  // else
-  return res;
+  return result;
 };
 
 export { myPow };
