b420c0ed247e66c178ea1e8e55a1a95ac272b086
rickvianaldi <rickvianaldi@gmail.com>
2026-04-24 12:08:58 +0700

    🔵feat: Refactor combine function for improved clarity; enhance comments for better understanding of the combination generation logic

--- leetcode-playground/0077-combinations.js
+++ leetcode-playground/0077-combinations.js
@@ -8,8 +8,36 @@
  * @param {number} k
  * @return {number[][]}
  */
-var combine = function(n, k) {
-    // implement
+var combine = function (n, k) {
+  // implement
+
+  let stack = []; // use stack to track decision on current combination path exploration
+  let result = [];
+
+  function helper(start) {
+    //2
+    // start decision tree
+    // base case:
+    if (stack.length === k) {
+      // when stack is enough for capture
+      result.push([...stack]); // clone the state of stack.
+      return;
+    }
+
+    for (let i = start; i <= n; i++) {
+      // ^ i <= n because you started at 1 to includes n (this case n=4, 4 is included.)
+      // start decision tree
+      stack.push(i); // picked i (because the number of 1-n, if you need characters, access its index string[i])
+      //! when you pick next number, it must be i+1, cos combination don't allow duplicate pick:
+      helper(i + 1); // in case order matters, reuse allowed, pass same i,
+      // incase order does not matter, no reuse, pass i+1 (skip over that character)
+      stack.pop(); // [1,3]
+    }
+  }
+
+  helper(1);
+
+  return result;
 };
 
-export { combine }
+export { combine };
