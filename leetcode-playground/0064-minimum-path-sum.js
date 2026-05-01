08330323e87bb08165a63bbf5287c9532273f9cf
rickvianaldi <rickvianaldi@gmail.com>
2026-05-01 10:35:29 +0000

    feat: US-033 - Create test cases for 0064-minimum-path-sum

--- leetcode-playground/0064-minimum-path-sum.js
+++ leetcode-playground/0064-minimum-path-sum.js
@@ -5,6 +5,12 @@
  *
  * @param {number[][]} grid
  * @return {number}
+ *
+ * @constraints
+ * - m == grid.length
+ * - n == grid[0].length
+ * - 1 <= m, n <= 200
+ * - 0 <= grid[i][j] <= 200
  */
 var minPathSum = function(grid) {
     // implement
